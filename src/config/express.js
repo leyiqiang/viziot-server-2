473cfee692e8c312a4cceb5a58949e2516fb9a62
ChenxiYuan <yuanchenxi95@gmail.com>
2018-10-12 02:52:05 -0400

    cors

--- src/config/express.js
+++ src/config/express.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 module.exports = app => {
+  app.use(cors())
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
 }
